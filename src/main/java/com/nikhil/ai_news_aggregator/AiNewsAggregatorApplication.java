65a53cd81b440fc785176226097085eb35005f83
Nikhil T D <dnikhil0306@gmail.com>
2025-08-09 19:55:16 +0530

    feat: Initialise Spring Boot application

---
+++ src/main/java/com/nikhil/ai_news_aggregator/AiNewsAggregatorApplication.java
@@ -0,0 +1,13 @@
+package com.nikhil.ai_news_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiNewsAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiNewsAggregatorApplication.class, args);
+	}
+
+}
